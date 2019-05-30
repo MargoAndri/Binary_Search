348b705f2e24d7b5c0da82f0e88af6512cbd8368
MargoAndri <margoandriasyan@gmail.com>
2019-05-30 23:13:07 +0300

    Initial commit

---
+++ binary_search.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @return {number}
+ */
+const compareNumbers = (firstNumber, secondNumber) => {
+    if (firstNumber > secondNumber) {
+        return 1;
+    }
+    if (firstNumber < secondNumber) {
+        return -1;
+    }
+};
+
+// Функия возвращает индекс элемента в массиве
+// Если функция возвращает -1, значит элемента в массиве нет
+/**
+ *
+ * @param {Array} arr
+ * @param {number} targetValue
+ * @return {number} index of targetValue
+ */
+const doBinarySearch = (arr, targetValue) => {
+    let start = 0;
+    let end = arr.length - 1;
+
+    arr.sort(compareNumbers);
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (targetValue === arr[middle]) {
+            return middle;
+        }
+        if (targetValue < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
+
+    return -1;
+};
+
+
+// Пример использования функции
+let array = [1, 3, 4, 2, 55, 65, 34, 1];
+let elementToFind = 54;
+doBinarySearch(array, elementToFind);
